e4855035d5ed11c90ac3bc1cd613853bb3c96e3a
Jose Sanrindo <49176895+TakashiJose@users.noreply.github.com>
2023-02-15 00:16:22 -0800

    Ping function

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits} = require('discord.js');
 require('dotenv').config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client(
+    { intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ] 
+});
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-});
+  });
+  
+  client.on('messageCreate', (message) => {
+    if(message.content === 'ping'){
+        message.reply('pong');
+    }
+  });
+  
 
 client.login(process.env.DISCORD_TOKEN);
