833f3de1b7c5a26aaed406f45535f92c4a89d9bb
Jose Sanrindo <49176895+TakashiJose@users.noreply.github.com>
2023-02-14 23:16:27 -0800

    as

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-const {Client, GatewayIntentBits} = require('dicsord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const client=new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
+const client = new Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('ready', ()=>{
-    console.log('The bot is ready');
-})
-
-client.on('messageCreate', message=>{
-    if(message.content==='ping'){
-        message.reply('pong');
-    }
-})
-
-client.login(process.env.TOKEN);
