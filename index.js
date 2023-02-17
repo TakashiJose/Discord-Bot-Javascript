18469bba609ecd007da410c19be5d8a8fea6f3a1
Jose Sanrindo <49176895+TakashiJose@users.noreply.github.com>
2023-02-17 00:46:54 -0800

    Event Handling

---
+++ index.js
@@ -0,0 +1,23 @@
+//Discord.js Packages
+const { Client, GatewayIntentBits, Partials, Collection} = require('discord.js'); 
+const {Guilds, GuildMembers, GuildMessages} = GatewayIntentBits;
+const {User, Message, GuildMember, ThreadMember} = Partials;
+
+const client= new Client({
+  intents: [Guilds, GuildMembers, GuildMessages], 
+  partials: [User, Message, GuildMember, ThreadMember],
+});
+
+//Third Party Packages
+require('dotenv').config();
+
+//Local Packages
+const{loadEvents} = require("./Handlers/eventHandler");
+
+
+client.events = new Collection();
+loadEvents(client);
+
+
+//Start Discord
+client.login(process.env.DISCORD_TOKEN);
